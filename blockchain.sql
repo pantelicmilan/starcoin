3fe6298afbaf54290fb74752070e16bcc8b2b3d5
Milan Pantelic <mpantelic39@gmail.com>
2018-04-28 20:39:55 +0200

    Dodavanje blockchain strukture.
    28.4.2018 - 20:39

---
+++ blockchain.sql
@@ -0,0 +1,15 @@
+#import:crypto:lambda:function
+#fetch_array
+#block[complete];
+complete = x.ipAdress.miner);
+
+
+
+CREATE TABLE blockchain (
+id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+hash VARCHAR(255) NOT NULL,
+prevHash VARCHAR(255) NOT NULL,
+od VARCHAR(255) NOT NULL,
+do VARCHAR(255) NOT NULL,
+reg_date TIMESTAMP
+)
